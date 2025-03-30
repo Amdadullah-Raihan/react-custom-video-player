bc06bd3eae903aaa199b6f2c7592ce3851de81ee
Amdadul Islam <amdadullahrayhan@gmail.com>
2025-03-30 18:36:04 +0600

    Enhance VideoPlayer component: add volume control with slider and update layout for better accessibility

--- src/pages/VideoPlayer.jsx
+++ src/pages/VideoPlayer.jsx
@@ -150,11 +150,28 @@ const VideoPlayer = () => {
             <line x1="19" x2="19" y1="5" y2="19" />
           </svg>
         </button>
-        <div>
-          <p className="">
-            <span>{formatTime(videoRef?.current?.currentTime)}</span> /{" "}
-            <span>{formatTime(videoRef?.current?.duration)}</span>
-          </p>
+        <p className="">
+          <span>{formatTime(videoRef?.current?.currentTime)}</span> /{" "}
+          <span>{formatTime(videoRef?.current?.duration)}</span>
+        </p>
+        <div className="flex items-center gap-2 ml-6">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-volume2-icon lucide-volume-2"
+          >
+            <path d="M11 4.702a.705.705 0 0 0-1.203-.498L6.413 7.587A1.4 1.4 0 0 1 5.416 8H3a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h2.416a1.4 1.4 0 0 1 .997.413l3.383 3.384A.705.705 0 0 0 11 19.298z" />
+            <path d="M16 9a5 5 0 0 1 0 6" />
+            <path d="M19.364 18.364a9 9 0 0 0 0-12.728" />
+          </svg>
+          <input type="range" name="" id="" />
         </div>
       </div>
       <video
