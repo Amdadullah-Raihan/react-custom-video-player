f34ee6b460145c519bb79acd08f522302bdb52fa
Amdadul Islam <amdadullahrayhan@gmail.com>
2025-04-04 22:44:46 +0600

    chore: remove tailwindcss plugin from Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss],
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: "src/index.js",
+      name: "ReactVideoPlayer",
+      fileName: "index",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
 });
