741561aafed41a8af417db6820b585d51b5d8006
Amdadul Islam <amdadullahrayhan@gmail.com>
2025-04-04 22:51:27 +0600

    Refactor code structure for improved readability and maintainability

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import VideoPlayer from "./modules/VideoPlayer";
+import VideoPlayer from "./VideoPlayer";
+import video from "./assets/video.mp4";
 
 function App() {
   return (
     <main>
-      <VideoPlayer title="JavaScript Basics (Variables, functions, events, DOM manipulation)." />
+      <VideoPlayer
+        title="JavaScript Basics (Variables, functions, events, DOM manipulation)."
+        src={video}
+      />
     </main>
   );
 }
